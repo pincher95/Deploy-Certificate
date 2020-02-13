9c0d5adbb30a47f70e8df595b0323ed781389c77
pincher95 <yuri.tsuprun@gmail.com>
2020-02-13 15:08:03 +0200

    bug fix

--- ssl-deploy.py
+++ ssl-deploy.py
@@ -5,19 +5,28 @@ from jumpssh import SSHSession
 
 @click.command()
 @click.option('-f', '--path-pfx-file', default='', help="Path to pfx file.")
-@click.option('-p', '--password', default='', help="Password phrase.")
-@click.option('-r','--remote-host',help="Remote Host.")
-@click.option('-b', '--bastion', defaulf="192.168.2.200", help="Bastion host.")
+@click.option('-p', '--password', default='', help="pfx password.")
+@click.option('-r', '--remote-host', default='', help="Remote Host.")
+# @click.option('-t', '--file-type', type=click.Choice(['PFX', 'PEM'], case_sensitive=False))
+@click.option('-b', '--bastion', default='192.168.2.200', help="Bastion host.", show_default=True)
 def check_pfx(path_pfx_file, password, bastion, remote_host):
     p12 = crypto.load_pkcs12(open(path_pfx_file, 'rb').read(), password)
     click.echo(crypto.dump_privatekey(crypto.FILETYPE_PEM, p12.get_privatekey()))
     click.echo(crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate()))
+    private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, p12.get_privatekey())
+    public_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate())
+    # ca_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_ca_certificates())
     # click.echo(path_pfx_file)
-    remote_ssl_backup(bastion, remote_host)
+    # remote_ssl_backup(bastion, remote_host)
+    gateway_session = SSHSession(host=bastion, port=7022, password=None,
+                                 missing_host_key_policy=None, username='support')
+    remote_session = gateway_session.get_remote_session(remote_host, password=None, username='root', port=7022)
+    print(remote_session.get_cmd_output('ls -lta'))
 
-
-def remote_ssl_backup(bastion, remote_host):
-    gateway_session = SSHSession(private_key_file='', host=bastion, port=7022, password=None,missing_host_key_policy='')
+# def remote_ssl_backup(bastion, remote_host):
+#     click.echo(bastion)
+#     click.echo(remote_host)
+# gateway_session = SSHSession(private_key_file='', host=bastion, port=7022, password=None, missing_host_key_policy='')
 
 
 def main():
