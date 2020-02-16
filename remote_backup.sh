a326b95bb8bdbc994b094a04211c5c991ea960fc
pincher95 <yuri.tsuprun@gmail.com>
2020-02-16 15:13:21 +0200

    bug fix

--- remote_backup.sh
+++ remote_backup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-#cd /etc/apache/passl
-mkdir /etc/apache/passl/backup-$(date +'%F') 2>/dev/null
-if [ -e /etc/apache/passl/pa.cert.intermediate ]; then
-  cp /etc/apache/passl/{pa.cert.cert,pa.cert.key,pa.cert.intermediate} backup-$(date +'%F')/ 2>/dev/null
-  cat /etc/apache/passl/{pa.cert.cert,pa.cert.key,pa.cert.intermediate} > /var/qmail/control/clientcert.pem 2>/dev/null
+SSL_PATH='/etc/apache/passl'
+QMAIL_PATH='/var/qmail/control'
+
+mkdir $SSL_PATH/backup-$(date +'%F') 2>/dev/null
+if [ -e $SSL_PATH/pa.cert.intermediate ]; then
+  cp $SSL_PATH/{pa.cert.cert,pa.cert.key,pa.cert.intermediate} $SSL_PATH/backup-$(date +'%F')/ 2>>/root/debug
+  cat $SSL_PATH/{pa.cert.cert,pa.cert.key,pa.cert.intermediate} > $QMAIL_PATH/clientcert.pem 2>/dev/null
 else
-  cp /etc/apache/passl/{pa.cert.cert,pa.cert.key} backup-$(date +'%F')/ 2>/dev/null
-  cat /etc/apache/passl/{pa.cert.cert,pa.cert.key} > /var/qmail/control/clientcert.pem 2>/dev/null
+  cp $SSL_PATH/{pa.cert.cert,pa.cert.key} $SSL_PATH/backup-$(date +'%F')/ 2>/dev/null
+  cat $SSL_PATH/{pa.cert.cert,pa.cert.key} > $QMAIL_PATH/clientcert.pem 2>/dev/null
 fi
-cp -p /var/qmail/control/clientcert.pem /var/qmail/control/clientcert.pem.back
-cp -p /var/qmail/control/servercert.pem /var/qmail/control/servercert.pem.back
-cat '' >> /var/qmail/control/clientcert.pem
-cp /var/qmail/control/clientcert.pem /var/qmail/control/servercert.pem
+cp -p $QMAIL_PATH/clientcert.pem $QMAIL_PATH/clientcert.pem.back
+cp -p $QMAIL_PATH/servercert.pem $QMAIL_PATH/servercert.pem.back
+cat '' >> $QMAIL_PATH/clientcert.pem
+cp $QMAIL_PATH/clientcert.pem $QMAIL_PATH/servercert.pem
 for i in {1..3}; do qmail stop; done; qmail start
 qmail start
 apachectl -k restart
--- ssl-deploy.py
+++ ssl-deploy.py
@@ -7,38 +7,28 @@ from jumpssh import SSHSession
 @click.option('-f', '--path-pfx-file', default='', help="Path to pfx file.")
 @click.option('-p', '--password', default='', help="pfx password.")
 @click.option('-r', '--remote-host', default='', help="Remote Host.")
-# @click.option('-t', '--file-type', type=click.Choice(['PFX', 'PEM'], case_sensitive=False))
 @click.option('-b', '--bastion', default='192.168.2.200', help="Bastion host.", show_default=True)
 def check_pfx(path_pfx_file, password, bastion, remote_host):
     p12 = crypto.load_pkcs12(open(path_pfx_file, 'rb').read(), password)
-    click.echo(crypto.dump_privatekey(crypto.FILETYPE_PEM, p12.get_privatekey()))
-    click.echo(crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate()))
     private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, p12.get_privatekey())
     public_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate())
-    try:
-        ca_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_ca_certificates())
+    # try:
+    #     ca_certificate = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_ca_certificates())
 
     gateway_session = SSHSession(host=bastion, port=7022, password=None,
                                  missing_host_key_policy=None, username='support')
     remote_session = gateway_session.get_remote_session(remote_host, password=None, username='root', port=7022)
-    # command_file = open("remote_backup.sh", "r")
-    # command_file_lines = command_file.readline()
-    # while command_file_lines:
-    #     print(command_file_lines)
-    #     command_file_lines = command_file.readline()
-    #     remote_session.run_cmd(command_file_lines)
-    # command_file.close()
     remote_session.put('./remote_backup.sh', '/root/remote_backup.sh', owner='root', permissions='0700')
     remote_session.run_cmd('/root/remote_backup.sh')
     remote_session.file(remote_path='/etc/apache/passl/pa.cert.key', content=private_key, owner='root',
                         permissions='644')
     remote_session.file(remote_path='/etc/apache/passl/pa.cert.cert', content=public_certificate, owner='root',
                         permissions='644')
-    remote_session.file(remote_path='/etc/apache/passl/pa.cert.intermediate', content=ca_certificate, owner='root',
-                        permissions='644')
+    # remote_session.file(remote_path='/etc/apache/passl/pa.cert.intermediate', content=ca_certificate, owner='root',
+    #                     permissions='644')
     print(remote_session.get_cmd_output('ls -alh'))
     print(remote_session.get_cmd_output("ls -alh /etc/apache/passl/backup-$(date +'%F')"))
-    print(remote_session.get_cmd_output('ls -alh /var/qmail/conrtol/'))
+    print(remote_session.get_cmd_output('ls -alh /var/qmail/control/'))
 
     remote_session.close()
     gateway_session.close()
